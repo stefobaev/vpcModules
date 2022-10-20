d3629c7ad5b9a011250f5a636a61a26fc13b9356
Stefan Baev <79969033+stefobaev@users.noreply.github.com>
2022-10-20 15:33:12 +0300

    Add files via upload

--- project/main.tf
+++ project/main.tf
@@ -3,5 +3,19 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "../modules"
+  source = "../modules/vpc"
+}
+
+module "alb" {
+  source = "../modules/alb/"
+  vpcId = module.vpc.vpcId
+  publicSubnet = module.vpc.publicSubnet
+}
+
+module "clu" {
+  source = "../modules/clu"
+  vpcId = module.vpc.vpcId
+  privateSubnet = module.vpc.privateSubnet
+  iamRole = module.clu.iamRole
+  listener = module.alb.listener
 }
